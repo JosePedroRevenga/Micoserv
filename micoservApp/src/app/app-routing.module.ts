514b1a3f0b8866e95a6cc86014cee86f37dacb1e
Aitor Portatil <a.solana23@gmail.com>
2019-06-06 15:06:24 +0200

    Creacion perfil
    Creacion del componente perfil

--- micoservApp/src/app/app-routing.module.ts
+++ micoservApp/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PerfilComponent } from './Components/perfil/perfil.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profile' , component: PerfilComponent, pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- micoservApp/src/app/app.module.ts
+++ micoservApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PerfilComponent } from './Components/perfil/perfil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule,
